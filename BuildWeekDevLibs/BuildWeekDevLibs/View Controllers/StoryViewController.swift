1ce24bbc258aab214c5a58860dec350e2e3009ec
bfvilchez <bfvilchez@icloud.com>
2019-08-27 08:24:11 -0600

    transferred project to my github

---
+++ BuildWeekDevLibs/BuildWeekDevLibs/View Controllers/StoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StoryViewController.swift
+//  BuildWeekDevLibs
+//
+//  Created by brian vilchez on 8/27/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
